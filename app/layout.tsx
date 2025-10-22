2f14bc6da7a88a15fb99186350a7b3b464cd60ca
munawar-queper <munawar@queper.co>
2025-10-23 00:36:35 +0500

    added g4 tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,6 +25,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics 4 Tag */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-T919SKQS5W"
+        />
+        <script id="google-analytics">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-T919SKQS5W');
+          `}
+        </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
